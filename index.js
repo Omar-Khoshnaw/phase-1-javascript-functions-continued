b27cc4666c28a9c76058da7cce6f1f5f2bf9776e
Omar Sardar <omar.khoshnaw@gmail.com>
2024-03-11 20:37:14 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+};
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+
+function wrapAdjective(par) {
+    if (par === "*") {
+      return function (x = "special") {
+        return `You are *${x}*!`;
+      };
+    } else if (par === "||") {
+      return function (x = "special") {
+        return `You are ||${x}||!`;
+      };
+    }
+  };
+
+
+const encouragingPromptFunction = wrapAdjective();
